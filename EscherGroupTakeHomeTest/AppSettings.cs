939b277741e96060ec6d90181373b1ea231aa5d0
Bellamy-Git <bellamygutierrez14@gmail.com>
2024-12-17 15:08:04 -0500

    added comments

--- EscherGroupTakeHomeTest/AppSettings.cs
+++ EscherGroupTakeHomeTest/AppSettings.cs
@@ -1,5 +1,8 @@
 ﻿namespace BellamyGutierrezEscher
 {
+
+    //this file is for deserialization
+    //AppSettings class is used to deserialize the JSON configuration data in C# objects
     public class AppSettings
     {
         public FilePaths FilePaths { get; set; }
